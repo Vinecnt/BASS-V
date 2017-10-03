aecd252f01b4bbbde7dea6c23f1f9bd1551b9bbb
Vincent Tran <vincenttran7411@gmail.com>
2017-10-03 00:40:48 -0400

    unzip group zip and remove all other files but readme

---
+++ project1/groupProject/data/morris-data.js
@@ -0,0 +1,58 @@
+$(function() {
+
+
+
+    Morris.Donut({
+        element: 'morris-donut-chart',
+        data: [{
+            label: "CompSci326",
+            value: 6
+        }, {
+            label: "CompSci220",
+            value: 5
+        }, {
+            label: "CompSci121",
+            value: 2
+        }],
+        resize: true
+    });
+
+    Morris.Bar({
+        element: 'morris-bar-chart',
+        data: [{
+            y: '2006',
+            a: 100,
+            b: 90
+        }, {
+            y: '2007',
+            a: 75,
+            b: 65
+        }, {
+            y: '2008',
+            a: 50,
+            b: 40
+        }, {
+            y: '2009',
+            a: 75,
+            b: 65
+        }, {
+            y: '2010',
+            a: 50,
+            b: 40
+        }, {
+            y: '2011',
+            a: 75,
+            b: 65
+        }, {
+            y: '2012',
+            a: 100,
+            b: 90
+        }],
+        xkey: 'y',
+        ykeys: ['a', 'b'],
+        labels: ['Series A', 'Series B'],
+        hideHover: 'auto',
+        resize: true
+    });
+
+});
