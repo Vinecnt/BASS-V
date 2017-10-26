bc5e24de9172a1487952af8ce9f437966b154303
Vincent Tran <vincenttran7411@gmail.com>
2017-10-26 15:35:21 -0400

    database fix

--- project1/bassv/taco/admin.py
+++ project1/bassv/taco/admin.py
@@ -1,57 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Message, Course, AssignmentCommunication, Assignment, TA, Professor, CourseOffering, MessageCommunication
 
-from .models import Assignments, AssignmentCommunication, Course, TA, Professor,  CourseOffering, Message, MessageCommunication
-
-# admin.site.register(Book)
-# admin.site.register(Author)
-
-# class BooksInstanceInline(admin.TabularInline):
-#     model = BookInstance
-#     extra = 0
-#
-# class BookInline(admin.TabularInline):
-#     model = Book
-#     extra = 0
-#
-@admin.register(Assignments)
-class AssignmentAdmin(admin.ModelAdmin):
-    list_display =  ('aid','aname')
-
-
-@admin.register(AssignmentCommunication)
-class AssignmentCommunicationAdmin(admin.ModelAdmin):
-    pass
-
-# admin.site.register(BookInstance)
-#
-# # Register the Admin classes for Book using the decorator
-#
-
-# Register the Admin classes for BookInstance using the decorator
-
-@admin.register(Course)
-class CourseAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(CourseOffering)
-class CourseOfferingAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(Message)
-class MessageAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(MessageCommunication)
-class MessageCommunication(admin.ModelAdmin):
-    pass
-
-# admin.site.register(AssignmentCommunication)
-# admin.site.register(Assignments)
-# admin.site.register(Course)
-# admin.site.register(TA)
-# admin.site.register(Professor)
-# admin.site.register(CourseOffering)
-# admin.site.register(MessageCommunication)
-# admin.site.register(Message)
+admin.site.register(Message)
+admin.site.register(Course)
+admin.site.register(TA)
+admin.site.register(Assignment)
+admin.site.register(AssignmentCommunication)
+admin.site.register(Professor)
+admin.site.register(CourseOffering)
+admin.site.register(MessageCommunication)
--- project1/bassv/taco/models.py
+++ project1/bassv/taco/models.py
@@ -1,28 +1,30 @@
 from django.db import models
-from django.urls import reverse  # Used to generate URLs by reversing the URL patterns
-import uuid  # Required for unique book instances
+import uuid
 
 # Create your models here.
-class Assignments(models.Model):
-    """
+
+
+class Assignment(models.Model):
+	"""
     int for AID, String
     Model representing a book genre (e.g. Science Fiction, Non Fiction).
     """
-    # unique id for each assignment 
-    aid = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for the Assignment")
-    # for stroing the link to the place where the assignment is located.
-    assignment_link = models.CharField(max_length=200, help_text="Enter the link to find the Assignment")
-    # foregin key to reference the course which the assifnment is for
-    cid= models.ForeignKey('Course',on_delete=models.SET_NULL, null=True)
-    # Assignment Name for referencing the assignment
-    aname = models.CharField(max_length=200, help_text="Entera name for the Assignment")
+	# unique id for each assignment
+	aid = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for the Assignment")
+	# for stroing the link to the place where the assignment is located.
+	assignment_link = models.CharField(max_length=200, help_text="Enter the link to find the Assignment")
+	# foregin key to reference the course which the assifnment is for
+	cid = models.ForeignKey('Course', on_delete=models.SET_NULL, null=True)
+	# Assignment Name for referencing the assignment
+	aname = models.CharField(max_length=200, help_text="Entera name for the Assignment")
 
-    def __str__(self):
-        """
+	def __str__(self):
+		"""
         String for representing the Model object (in Admin site etc.)
         Returns the name of the Assignment
         """
-        return self.aname
+		return self.aname
+
 
 class AssignmentCommunication(models.Model):
 	"""
@@ -31,68 +33,74 @@ class AssignmentCommunication(models.Model):
 	TA ID
 	PROFESSOR ID
 	"""
-	aid= models.ForeignKey('Assignments',on_delete=models.SET_NULL, null=True)
-	tid= models.ForeignKey('TA',on_delete=models.SET_NULL, null=True)
-	pid= models.ForeignKey('Professor',on_delete=models.SET_NULL, null=True)
+	aid = models.ForeignKey('Assignment', on_delete=models.SET_NULL, null=True)
+	tid = models.ForeignKey('TA', on_delete=models.SET_NULL, null=True)
+	pid = models.ForeignKey('Professor', on_delete=models.SET_NULL, null=True)
+
 
 class Course(models.Model):
-	# unique id for each assignment 
-    cid = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for the Course")
-    cname= models.CharField(max_length=200, help_text="Enter Course Name")
+	# unique id for each assignment
+	cid = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for the Course")
+	cname = models.CharField(max_length=200, help_text="Enter Course Name")
+
 
 class TA(models.Model):
 	# unique id for each assignment
-    tid = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for the TA")
-    tname = models.CharField(max_length=200, help_text="Enter First Name")
-    courseOfferingID = models.ManyToManyField(Course, help_text="Select a course assigned to TA")
+	tid = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for the TA")
+	tname = models.CharField(max_length=200, help_text="Enter First Name")
+	courseOfferingID = models.ManyToManyField(Course, help_text="Select a course assigned to TA")
 
-    def display_course(self):
-        """
+	def display_course(self):
+		"""
         Creates a string for the courses. This is required to display courses for TA.
         """
-        return ', '.join([ courseOfferingID.name for courseOfferingID in self.courseOfferingID.all()[:3] ])
-        display_course.short_description = 'Courses assigned to this TA'
+		return ', '.join([courseOfferingID.name for courseOfferingID in self.courseOfferingID.all()[:3]])
+		display_course.short_description = 'Courses assigned to this TA'
 
-    def __str__(self):
-        """
+	def __str__(self):
+		"""
         String for representing the Model object.
         Returns the name of the TA and their unique ID.
         """
-        return '%s, %s' % (self.tname, self.tid)
+		return '%s, %s' % (self.tname, self.tid)
+
 
 class Professor(models.Model):
-    pid = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for the Professor")
-    pname = models.CharField(max_length=200, help_text="Enter Professor's Name")
-    courseOfferingID = models.ManyToManyField(Course, help_text="Select a course by this Professor")
+	pid = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for the Professor")
+	pname = models.CharField(max_length=200, help_text="Enter Professor's Name")
+	courseOfferingID = models.ManyToManyField(Course, help_text="Select a course by this Professor")
 
-    def display_course(self):
-        """
+	def display_course(self):
+		"""
         Creates a string for the courses. This is required to display courses in Admin.
         """
-        return ', '.join([ courseOfferingID.name for courseOfferingID in self.courseOfferingID.all()[:3] ])
-        display_course.short_description = 'Courses taught by this Professor'
+		return ', '.join([courseOfferingID.name for courseOfferingID in self.courseOfferingID.all()[:3]])
+		display_course.short_description = 'Courses taught by this Professor'
 
-    def __str__(self):
-        """
+	def __str__(self):
+		"""
         String for representing the Model object.
         Returns the name of the professor and their unique ID.
         """
-        return '%s, %s' % (self.pname, self.id)
+		return '%s, %s' % (self.pname, self.id)
+
 
 class CourseOffering(models.Model):
-	cid=models.ForeignKey('Course',on_delete=models.SET_NULL, null=True)
-	tid= models.ForeignKey('TA',on_delete=models.SET_NULL, null=True)
-	pid= models.ForeignKey('Professor',on_delete=models.SET_NULL, null=True)
+	cid = models.ForeignKey('Course', on_delete=models.SET_NULL, null=True)
+	tid = models.ForeignKey('TA', on_delete=models.SET_NULL, null=True)
+	pid = models.ForeignKey('Professor', on_delete=models.SET_NULL, null=True)
+
 
 class Message(models.Model):
-	mid= models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for the Message")
-	mstring= models.CharField(max_length=200, help_text="Enter the Message")
+	mid = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for the Message")
+	mstring = models.CharField(max_length=200, help_text="Enter the Message")
+
 
 class MessageCommunication(models.Model):
-	# I dont know how to reference two foreign keys to the same attribute so to make 
-	# it easier for the time being we just have TA and PID 
+	# I dont know how to reference two foreign keys to the same attribute so to make
+	# it easier for the time being we just have TA and PID
 	# also worst come worst we can hard code what we want our application to do
-	
-	mid=models.ForeignKey('Message',on_delete=models.SET_NULL, null=True)
-	tid= models.ForeignKey('TA',on_delete=models.SET_NULL, null=True)
-	pid= models.ForeignKey('Professor',on_delete=models.SET_NULL, null=True)
+
+	mid = models.ForeignKey('Message', on_delete=models.SET_NULL, null=True)
+	tid = models.ForeignKey('TA', on_delete=models.SET_NULL, null=True)
+	pid = models.ForeignKey('Professor', on_delete=models.SET_NULL, null=True)
