50b67e47f930023d703b419a1052e655679258fa
Vincent Tran <vincenttran7411@gmail.com>
2017-10-31 18:51:36 -0400

    add second web page

--- project1/bassv/taco/views.py
+++ project1/bassv/taco/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from django.views import generic
 
 # Create your views here.
 def index(request):
@@ -15,8 +16,8 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     context = dict()
-    for item in class_iter:
-        context[str(item)] = item.cname
+    # for item in class_iter:
+    #     context[str(item)] = item.cname
 
     return render(
         request,
@@ -25,3 +26,7 @@ def index(request):
     #              'num_instances_available': num_instances_available, 'num_authors': num_authors},
         context,
     )
+
+class CourseListView(generic.ListView):
+    model = Course
+
