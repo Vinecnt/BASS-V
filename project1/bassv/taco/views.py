80ededc0257f37f02d4686fa7e7f60b9ded93db7
Vincent Tran <vincenttran7411@gmail.com>
2017-10-31 13:47:47 -0400

    add template and fixing views

--- project1/bassv/taco/views.py
+++ project1/bassv/taco/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def index(request):
+    """
+    View function for home page of site.
+    """
+    # # Generate counts of some of the main objects
+    class_iter = Course.objects.all()
+    # num_instances = BookInstance.objects.all().count()
+    # # Available books (status = 'a')
+    # num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    # num_authors = Author.objects.count()  # The 'all()' is implied by default.
+
+    # Render the HTML template index.html with the data in the context variable
+
+    return render(
+        request,
+        'index.html',
+    #     context={'num_books': num_books, 'num_instances': num_instances,
+    #              'num_instances_available': num_instances_available, 'num_authors': num_authors},
+        for item in class_iter:
+
+            context=(),
+    # )
