0d88563877ea1b9a95ca6d965dd04a0fd7984a12
Ani Gevorgyan <agevorgyan95@gmail.com>
2017-10-31 20:05:55 -0400

    add the course_detail

--- project1/bassv/taco/urls.py
+++ project1/bassv/taco/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^courses/$', views.CourseListView.as_view(), name='courses')
+    url(r'^courses/$', views.CourseListView.as_view(), name='courses'),
+    url(r'^course/(?P<pk>\d+)$', views.CourseDetailView.as_view(), name='course-detail'),
 ]
--- project1/bassv/taco/views.py
+++ project1/bassv/taco/views.py
@@ -30,3 +30,5 @@ def index(request):
 class CourseListView(generic.ListView):
     model = Course
 
+class CourseDetailView(generic.DetailView):
+    model = Course
